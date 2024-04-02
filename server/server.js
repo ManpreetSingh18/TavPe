dcdc39a2e2093e30e18eb0db4dc124b766ecc28b
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-04-02 21:18:08 +0530

    Contact Form backend
    In this we create backend of contact form like Schema, Route & Logics

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const express=require("express");
 const app=express();
-const router=require("./router/auth-router");
+const authRoute=require("./router/auth-router");
+const contactRoute=require("./router/contact-router");
 const connectDb=require("./utils/db");
 const errorMiddleware=require("./middleware/error-middleware");
+
 app.use(express.json());
 
 //Mount the router:To use the router in your main Express App
 // you can mount it at a specific URL prefix
-app.use("/api/auth",router);
+app.use("/api/auth",authRoute);
 //after above code it will jump to auth-router and will not see below
 
+app.use("/api/form",contactRoute);
 //it means  that if there error occured then we will not proceed with making connection in next code 
 //s //app.use(errorMiddleware());
 app.use(errorMiddleware);
