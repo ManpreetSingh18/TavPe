d0a366b56826e7fba6ea35ae220b1a9f841a15db
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-04-21 00:24:28 +0530

    home page & contact form
    Complete Home Page and working contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,3 +1,105 @@
-export const Contact =() =>{
-    return <h1>Hello from contact page</h1>
-}
+import { useState } from "react";
+
+export const Contact = () => {
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  // lets tackle our handleInput
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+
+  // handle fomr getFormSubmissionInfo
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(contact);
+  };
+
+//  Help me reach 1 Million subs 👉 https://youtube.com/thapatechnical
+
+  return (
+    <>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">contact us</h1>
+        </div>
+        {/* contact page main  */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="/images/support.png" alt="we are always ready to help" />
+          </div>
+
+          {/* contact form content actual  */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message">message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  cols="30"
+                  rows="6"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <section className="mb-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.228644744675!2d77.27068057495754!3d28.652871783157725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfcbccf7bd899%3A0xf49976cff4de648f!2sLovely%20Associates.!5e0!3m2!1sen!2sin!4v1713638730227!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </section>
+      </section>
+    </>
+  );
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,101 @@
+// import { Analytics } from "../components/Analytics";
 
-export const Home =() =>{
+export const Home = () => {
   return (
     <>
-      
-      <h1>Hello from home page</h1>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>We are the World Best Loan Company</p>
+              <h1>Welcome to TavPrasad</h1>
+              <p>
+                Are you ready to take your business to the next level with
+                cutting-edge loan solutions? Look no further! At Loanxe,
+                we specialize in providing innovative IT services and solutions
+                tailored to meet your unique needs.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">connect now</button>
+                </a>
+                <a href="/services">
+                  <button className="btn secondary-btn">learn more</button>
+                </a>
+              </div>
+            </div>
+
+            {/* hero images  */}
+            <div className="hero-image">
+              <img
+                src="https://images.unsplash.com/photo-1633158829585-23ba8f7c8caf?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="coding together"
+                width="400"
+                height="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* 2nd section  */}
+
+      <section className="section-analytics">
+        <div className="container">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Registered Company</p>
+          </div>
+          <div calssName="div1">
+            <h2>100,00+</h2>
+            <p>Happy Clients</p>
+          </div>
+          <div calssName="div1">
+            <h2>500+</h2>
+            <p>Well Known Developers</p>
+          </div>
+          <div calssName="div1">
+            <h2>24/7</h2>
+            <p>Service</p>
+          </div>
+
+        </div>
+      </section>
+      {/* <Analytics /> */}
+
+      {/* 3rd section  */}
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          {/* hero images  */}
+          <div className="hero-image">
+            <img
+              src="https://images.unsplash.com/photo-1633158829585-23ba8f7c8caf?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="coding together"
+              width="400"
+              height="500"
+            />
+          </div>
+
+          <div className="hero-content">
+            <p>We are here to help you</p>
+            <h1>Get Started Today</h1>
+            <p>
+              Ready to take the first step towards a more efficient and secure
+              IT infrastructure? Contact us today for a free consultation and
+              let's discuss how Loanxe can help your business thrive in
+              the digital age.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+                <button className="btn">connect now</button>
+              </a>
+              <a href="/services">
+                <button className="btn secondary-btn">learn more</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
-}
+};
