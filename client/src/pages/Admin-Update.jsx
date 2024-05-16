ef86469e71f9172e98feca029b72250b102839d0
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-05-16 22:16:59 +0530

    User Data Updates
     React Form Handling, Toast Alerts

--- client/src/pages/Admin-Update.jsx
+++ client/src/pages/Admin-Update.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom"
 import {useAuth} from "../store/auth"
-
+import {  toast } from 'react-toastify';
 export const AdminUpdate = () => {
   const [data, setData] = useState({
     username: "",
@@ -41,7 +41,40 @@ export const AdminUpdate = () => {
     getSingleUserData();
   }, []);
 
-  const handleInput = () => {};
+  const handleInput = (e) => {
+    let name=e.target.name;
+    let value=e.target.value;
+    setData({
+     ...data,
+      [name]: value,
+    });
+  };
+
+  // to update the data dynamically
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/admin/users/update/${params.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: authorizationToken,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        toast.success("Updated successfully")
+      }else{
+        toast.error("Unsuccessful update")
+      }
+    } catch (error) {
+      console.log(error);
+      next(error)
+    }
+  };
 
   return (
     <>
@@ -53,7 +86,7 @@ export const AdminUpdate = () => {
         <div className="container grid grid-two-cols">
           {/*Admin user edit  content actual  */}
           <section className="section-form">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="username">username</label>
                 <input
