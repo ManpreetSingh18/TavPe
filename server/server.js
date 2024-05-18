9c2a35ea093f34f4da9e281f71c81cbc6788623d
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-05-18 23:59:22 +0530

    cors changes

--- server/server.js
+++ server/server.js
@@ -1,47 +1,41 @@
 require("dotenv").config();
-const express=require("express");
-const app=express();
-const authRoute=require("./router/auth-router");
-const contactRoute=require("./router/contact-router");
-const serviceRoute=require("./router/service-router");
-const adminRoute=require("./router/admin-router");
-const connectDb=require("./utils/db");
-const errorMiddleware=require("./middleware/error-middleware");
-const cors=require("cors");
-// let tackle cors
+const express = require("express");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const serviceRoute = require("./router/service-router");
+const adminRoute = require("./router/admin-router");
+const connectDb = require("./utils/db");
+const errorMiddleware = require("./middleware/error-middleware");
+const cors = require("cors");
+
+// Allow requests from specified origins
 const corsOptions = {
-    origin: ["http://localhost:5173","https://tav-prasad.vercel.app"],
-    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials: true,
-  };
+  origin: ["http://localhost:5173", "https://tav-prasad.vercel.app"],
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+
+// Use the CORS middleware with the specified options
 app.use(cors(corsOptions));
+
+// Parse JSON bodies
 app.use(express.json());
 
-//Mount the router:To use the router in your main Express App
-// you can mount it at a specific URL prefix
-app.use("/api/auth",authRoute);
-//after above code it will jump to auth-router and will not see below
+// Mount the routers
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+app.use("/api/data", serviceRoute);
+app.use("/api/admin", adminRoute);
 
-app.use("/api/form",contactRoute);
-//it means  that if there error occured then we will not proceed with making connection in next code 
-//s //app.use(errorMiddleware());
+// Error handling middleware
 app.use(errorMiddleware);
 
-//route for service
-app.use("/api/data",serviceRoute);
+// Connect to the database and start the server
+const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT is not specified in the environment variables
 
-//route for Admin
-app.use("/api/admin",adminRoute);
-
-
-
-const PORT=process.env.PORT;
-
-//agar connection DB se ho tab hi age connect kare
-connectDb().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server is running at port: ${PORT}`);
-    })
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+  });
 });
-
-
