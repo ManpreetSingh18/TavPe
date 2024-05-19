75bf0c6db9f8d53a6eb5e95cea7c49ee7dc7ca2a
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-05-19 14:48:05 +0530

    Update contact-router.js

--- server/router/contact-router.js
+++ server/router/contact-router.js
@@ -1,6 +1,12 @@
-const express=require("express");
-const router=express.Router();
-const contactForm=require("../controllers/contact-controller")
-router.route("/contact").post(contactForm);
-//router.route("/contact").post
-module.exports=router;
+const express = require("express");
+const router = express.Router();
+const cors = require("cors"); // Import the cors middleware
+const contactForm = require("../controllers/contact-controller");
+
+// Apply CORS middleware to the router
+router.use(cors());
+
+// Define route handler for POST request to /api/form/contact
+router.post("/contact", contactForm);
+
+module.exports = router;
